7ee20fd8653604e4e60f4b00ea1e71a3850cc145
xilesun <2013xile@gmail.com>
2019-06-18 21:28:23 +0800

    MOD: server

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ var serve = require('koa-static');
 
 const app = new Koa();
 
-app.use(async (ctx, next) => {
-  ctx.set("Access-Control-Allow-Origin", "*");
-  ctx.set("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE");
-  ctx.set("Access-Control-Allow-Headers", "x-requested-with, accept, origin, content-type");
-  await next();
-});
+// app.use(async (ctx, next) => {
+//   ctx.set("Access-Control-Allow-Origin", "*");
+//   ctx.set("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE");
+//   ctx.set("Access-Control-Allow-Headers", "x-requested-with, accept, origin, content-type");
+//   await next();
+// });
 app.use(serve('./public'));
 app.use(bodyParser());
 
